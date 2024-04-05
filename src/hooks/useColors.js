5d36cbcc16abe8320a050d5561b5a3e9006dc5b9
Didier Alejandro Fajardo Annear <didier.fajardo@uptc.edu.co>
2024-04-05 09:59:28 -0500

    Update useColors.js

--- src/hooks/useColors.js
+++ src/hooks/useColors.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { getColorList, getLastColor } from "../helpers/getColors";
 
-export const useColors = () => {
+export function useColors () {
     const [color, setColor] = useState(getLastColor());
     const [colorsList, setColorList] = useState(getColorList());
 
